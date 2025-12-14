ea07dfbac09a7e95d72ec2792b40518a88318e9c
root <root@ORDI-Ferdi>
2025-12-14 22:12:20 +0100

    Déplacement gauche droite avec le clavier

--- src/signal_handler.rs
+++ src/signal_handler.rs
@@ -31,18 +31,91 @@ pub mod SignalHandler {
         }
     }
 
-    fn print_new_command(current_command: &str, user: &str) {
+    fn print_new_command(current_command: &StringIndex, user: &str) {
         print!("\r\x1b[2K"); 
-        print!("{}{}", user, current_command);
+        let mut to_prompt = current_command.get().to_string();
+        to_prompt.push(' ');
+        print!("{}{}", user, to_prompt);
+        let move_cursor = to_prompt.len() - current_command.get_index();
+        print!("\x1b[{}D", move_cursor);
         std::io::stdout().flush().unwrap();
     }
 
+    #[derive(Debug)]
+    pub struct StringIndex {
+        string: String,
+        index: usize
+    }
+
+    impl StringIndex {
+        pub fn push_str(&mut self, c: &str) {
+            self.string.push_str(c);
+        }
+
+        pub fn insert(&mut self, c: char) {
+            self.string.insert(self.index, c);
+            self.index += 1;
+        }
+
+        pub fn insert_str(&mut self, c: &str) {
+            for char in c.chars() {
+                self.string.insert(self.index, char);
+                self.index+=1;
+            }
+        }
+
+        pub fn get_index(&self) -> usize {
+            self.index
+        }
+        pub fn remove(&mut self) {
+            if self.index > 0 {
+              self.string.remove(self.index-1);
+              self.index -= 1;
+            }
+        }
+        pub fn new() -> Self {
+            Self {string: String::new(), index:0}
+        }
+
+        pub fn from(c: &str) -> Self {
+            let string = String::from(c);
+            let index = c.len();
+            Self {string, index}
+        }
+
+        pub fn is_empty(&self) -> bool {
+            self.string.is_empty()
+        }
+
+        pub fn move_right(&mut self) {
+            if self.index < self.string.len() {
+                self.index += 1;
+            }
+        }
+
+        pub fn move_left(&mut self) {
+            if self.index > 0 {
+                self.index -= 1;
+            }
+        }
+
+
+        pub fn set(&mut self, c: String) -> () {
+            self.index = c.len();
+            self.string = c;
+        }
+
+        pub fn get(&self) -> &str {
+            &self.string
+        }
+
+    }
     pub async fn handle_command(shell_variables: &mut ShellVariables, user: &str) -> String {
         
 
 
         let history = shell_variables.get_history();
-        let mut current_command = String::new();
+        let mut current_command = StringIndex::new();
         let mut new_command = String::new();
         let mut history_index = 0;
         let min_index = user.len();
@@ -60,7 +133,7 @@ pub mod SignalHandler {
                 match event::read().unwrap() {
                     Event::Key(KeyEvent { code, modifiers, .. }) => match (code, modifiers) {
                         (KeyCode::Char('d'), KeyModifiers::CONTROL) => {
-                            if current_command == "" {
+                            if current_command.is_empty() {
                                 println!("^D");
                                 print!("\r\x1b[2D");
                                 stdout.flush().unwrap();
@@ -69,37 +142,28 @@ pub mod SignalHandler {
                             }
                         }
                         (KeyCode::Char('c'), KeyModifiers::CONTROL) => {
-                            current_command.push_str("^C");
+                            current_command.insert_str("^C");
                             print!("{}","^C");
                             break;
                         }
                         (KeyCode::Char(c), _) => {
-                            current_command.push(c);
+                            current_command.insert(c);
                             if history_index==0 {new_command.push(c);}
-                            print!("{}", c);
-                            stdout.flush().unwrap();
+                            print_new_command(&current_command, user);
                         }
                         (KeyCode::Enter, _) => {
-                            current_command.push('\n');
+                            current_command.push_str("\n");
                             print!("{}",'\n');
                             break;
                         }
                         (KeyCode::Backspace, _) => {
-                            if !current_command.is_empty() {
-                                if current_command.pop().is_some() {
-                                    if history_index == 0 {
-                                        new_command.pop();
-                                    }
-                                    print!("\x08 \x08"); // efface le dernier caractère
-                                    stdout.flush().unwrap();
-                                }
-                            }
-
+                            current_command.remove();
+                            print_new_command(&current_command, user);        
                         }
                         (KeyCode::Up, _)  => {
                             if history_index < history.len() {
                                 history_index += 1;
-                                current_command = history[history_index-1].clone();
+                                current_command.set(history[history_index-1].clone());
                             }
                             print_new_command(&current_command, &user);
                         }
@@ -107,17 +171,20 @@ pub mod SignalHandler {
                             if history_index>0 {
                                 history_index-=1;
                                 if history_index == 0 {
-                                    current_command = new_command.clone();
+                                    current_command.set(new_command.clone());
                                 } else {
-                                    current_command = history[history_index-1].clone();
+                                    current_command.set(history[history_index-1].clone());
                                 }
                                 print_new_command(&current_command, &user);
                             } 
                         }
                         (KeyCode::Left, _) => {
-                            
+                            current_command.move_left();
+                            print_new_command(&current_command, user);
                         }
                         (KeyCode::Right, _) => {
+                            current_command.move_right();
+                            print_new_command(&current_command, user);
                         }
                         _ => {}
                     },
@@ -129,6 +196,6 @@ pub mod SignalHandler {
         stdout.flush().unwrap();
         disable_raw_mode().unwrap();
         
-        current_command
+        current_command.get().to_string()
     }
 }
